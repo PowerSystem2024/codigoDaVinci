85d76056b682b34786b763ce8d79744f794f37ec
Jonathan Suarez <jony35622@gmail.com>
2024-10-23 22:15:14 -0300

    Agrego portafolio web grupal completado hasta la  clase 8

---
+++ Tecnicatura/SegundoSemestre/PortafolioWeb/Proyecto-curriculum/src/components/DatosPersonales.vue
@@ -0,0 +1,83 @@
+<script setup>
+const title = 'Leonardo Da Vinci';
+const descripcion = 'Multifacético  y ahora incursionando en la Tecnicatura Universitaria en programación de la UTN';
+const residencia = '-Villa Toscana de Vinci';
+const presentacion = 'Hola , bienvenidos a mi mundo.';
+
+const redesSociales = [
+    {id: 1, name:'Linkedin', src:'/src/assets/linkedin-icon.svg' , url: 'https://www.linkedin.com/in/Jonathan'},
+    {id: 2, name:'Instagram', src:'/src/assets/instagram-icon.svg' , url: ''},
+    {id: 3, name:'GitHub', src:'/src/assets/github-icon.svg' , url: ''},
+    {id: 4, name:'Curriculum', src:'/src/assets/curriculum-icon.svg' , url: ''},
+];
+
+const telefono = '1452';
+
+</script>
+
+<template>
+    <section class="datos-personales">
+        <div class="card">
+            <h1>{{title}}</h1>
+            <h2>{{descripcion}}</h2>
+            <p>{{presentacion}}</p>
+            <ul class="container-lista">
+                <li v-for="red in redesSociales" :key="red.id">
+                <a :href="red.url"><img class="icon-redsocial" :src="red.src" width="15rem" :alt="red.name" ></a>
+                </li>
+            </ul>
+            <h3>{{telefono}}</h3>
+            <h4>{{ residencia }}</h4>
+        </div>
+
+    </section>
+
+</template>
+
+<style scoped>
+h1{
+    font-size: 2.5rem;
+}
+
+p{
+    font-size: 1.2rem;
+    font-weight: 600;
+    margin-bottom: 1rem;
+}
+
+.card{
+background-color: rgb(28,41,52);
+border-radius: 10px;
+padding: 10px;
+margin:10px;
+text-align: center;
+}
+
+.container-lista{
+ display: flex;
+ justify-content: center;
+ list-style: none;
+ padding: 0;
+ margin: 0;
+ text-align: center;
+}
+
+.icon-redsocial{
+    align-items: center;
+    background-color: aliceblue;
+    border-radius: 50%;
+    padding: 2px;
+    margin: 5px;
+    box-shadow: 0 0 5px rgba(95, 124, 205, 0.934);
+}
+
+.icon-redsocial:hover {
+    background-color: rgb(28, 41, 52);
+    box-shadow: 0 0 5px rgba(251, 249, 249, 0.934);
+}
+
+h3{
+    font-size: 1.2rem;
+    font-weight: 600;
+}
+</style>
---
+++ Tecnicatura/SegundoSemestre/PortafolioWeb/Proyecto-curriculum/src/components/ProyectosComponentes.vue
@@ -0,0 +1,150 @@
+<script setup>
+</script>
+
+<template>
+    <div class="galeria">
+        <div class="proyecto">
+            <img src="/src/assets/img-mona.jpeg" alt="Proyecto 1">
+            <div class="proyecto-info">
+                <h3>Pintura Mona Modern</h3>
+                <p>Realicé una reversión de una de mis piezas clásicas. Fue toda una aventura</p>
+                <div class="proyecto-links">
+                    <a href="https://www.youtube.com/watch?v=yRK_uCMwZPY" class="btn-ver-mas" target="_blank" rel="nooper norefrrer">Ver Proyecto</a>
+                    <a href="" class="github-link" target="_blank"
+                    rel="noopener noreferrer">Ver código Github</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.galeria{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-wrap: wrap;/*distribuye las tarjetas que tenga dentreo del contenedor galeria */
+    gap: 20px; /*espacio entre tarjeta y tarjeta */
+    padding: 20px;/*agrega un padding de 20px alrededor de la galeria */
+    justify-content: center; /*las tarjetas quedaran centradas dentro de galeria */
+    background: linear-gradient(-45deg, #021526, #03346e, #6eacda, #E2E2B6);
+    background-size: 400% 400%;/*ajusta el tamaño del fodo para que cubra el área completa */
+    animation: gradient 15s ease infinite;
+}
+
+/*Define una animación llamada 'gradient' para cambiar la posiciónm del fondo */
+@keyframes gradient{
+    /*al inicio , el fondo está poosicionado en el 0% horizontal y 50% vertical */
+    0%{
+        background-position: 100% 50%;
+    }
+
+    /*a la mitad de la animación , el fondo se desplaza al 100% horizontal */
+    50% {
+        background-position: 100% 50%;
+    }
+
+    /* al final , el fondo vuelve a la posición inicial*/
+    100% {
+        background-position: 0% 50%;
+    }
+}
+
+
+    /*la clase 'proyecto' es el contenedor individual de cada proyecto */
+.proyecto {
+    display: flex;/*utiliza flexbox para organizar el contenido del proyecto en una columna */ 
+    flex-direction: column; /*establece la dirección de los elementos en columna */ 
+    border: 2px solid #ddd;/*aplica un borde de 2px solido y color gris claro (#ddd) alrededor del proyecto */      
+    border-radius: 8px;/*redondea las esquinas del contenedor */
+    overflow: hidden; /*oculta cualquier contenido que se desborde del contenedor */
+    background-color: #f9f9f9;
+    max-width: 222px;/*define el ancho máximo del proyecto en 222px */
+    flex: 1 1 300px;/* hace que el contenedor se ajuste flexiblemente ocupando al menos 300 px de ancho */
+    }
+
+    /*estilo para las imagenes dentro del contenedor 'proyecto' */
+.proyecto img{
+    /*hace que la imagen ocupe el 100% del ancho del contenedor */
+    width: 100%;
+    /*Mantiene la relación de aspecto de la imagen */
+    height: auto;
+    /*asegura que la imagen se muestre como un bloque, sin espacio en línea alrededor */
+    display: block;
+    }
+
+    /*La clase 'proyecto-info' contiene la información del proyecto*/
+    .proyecto-info{
+        /*Agrega un padding de 15px alrededor de la información */
+        padding: 15px;
+        /*Centra el texto dentro del contenedor */
+        text-align: center;
+    }
+
+    /*Estilo para los títulos de los proyectos */
+    .proyecto-info h3{
+    /*Establece un margen superior e inferior de 10px */
+     margin: 10px 0;
+     /*Define el tamaño de la fuente a 1.3em (relativo al tamaño de fuente del contenedor) */
+     font-size: 1.3rem;
+     /*aplica un color gris oscuro (#333) al texto */
+     color: #333;
+    }
+
+    /*Estilo para los párrafos de los proyectos */
+    .proyecto-info p{
+        /*Establece un margen superior e inferior de 10px */
+        margin: 10 px 0;
+        /*Define el tamaño de la fuente a 1em (igual al tamaño de fuente base) */
+        font-size: 1em;
+        /*Aplica un color gris (#666) al texto*/
+        color: #666;
+    }
+
+    /*La clase 'proyecto-links' organiza los enlaces y botones del proyecto */
+    .proyecto-links{
+        /*utiliza flexbox para organizar los enlaces en una columna */
+        display: flex;
+        /*Establece la dirección de los elementos en columna */
+        flex-direction: column;
+        /*Establece un espacio de 10px entre los elementos */
+        gap: 10px;
+        /*agrega un margen superior de 10px */
+        margin-top: 10px;
+    }
+
+    /*Estilo para el botón 'Ver más' dentro de la clase 'proyecto-links'*/
+    .proyecto-links .btn-ver-mas{
+        /*Aplica un color de fondo azul */
+        background-color: #007BFF;
+        /*Aplica un color de texto blanco */
+        color: #fff;
+        /*Agrega un padding de 10px en vertical y 15px en horizontal */
+        padding: 10px 15px;
+        /*Redondea las esquinas del botón */
+        border-radius: 5px;
+        /*Elimina el subrayado del texto */
+        text-decoration: none;
+        /*Aplica una transición suave al cambiar el color de fondo */
+        transition: background-color 0.3s;
+    }
+    /*Estilo para cuando el botón 'Ver más' es hover */ 
+    .proyecto-links .btn-ver-mas:hover{
+        background-color: #0056b3;
+    }
+
+    /*Estilo para el enlace a Github dentro de la clase 'proyecto-links' */
+    .proyecto-links .github-link{
+        /*aplica un co lor gris oscuro (#333) al texto */
+        color: #333;
+        /*Elimina el subrayado del texto */
+        text-decoration: none;
+        /*Establece el tamaño de fuente a 0.9em ( un poco más pequeño que el tamaño base) */
+        font-size: 0.9em;
+    }
+        /*Estilo para cuando el enlace de Github es hover */
+        .proyecto-links .github-link:hover {
+            /*añade un subrayado al texto */
+            text-decoration: underline;
+        }
+</style>
